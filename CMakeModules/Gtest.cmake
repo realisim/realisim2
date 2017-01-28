ab107a102faddc68296a1d9d9bf114f138361fef
Pierre-Olivier Beaudoin <pierreolivier.beaudoin@gmail.com>
2017-01-28 04:54:22 -0500

    Comments to GTest and math's unit tests

--- CMakeModules/Gtest.cmake
+++ CMakeModules/Gtest.cmake
@@ -1,6 +1,6 @@
 
 set( GTEST_DIR  ${CMAKE_SOURCE_DIR}/thirdParties/Gtest/v1.8.0 )
-message( "GTEST_DIR " ${GTEST_DIR} )
+#message( "GTEST_DIR " ${GTEST_DIR} )
 
 # add the include path...
 include_directories( ${GTEST_DIR}/include/ )
